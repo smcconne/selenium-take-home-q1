8e508a1f4c21cfcd1a58c5f5eb8a702dd1cb8450
Sage McConnell <smcconne@github.com>
2022-12-17 16:14:03 -0400

    Implement unittest for login tests

--- login_tests.py
+++ login_tests.py
@@ -1,22 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from faker import Faker
 import unittest
 import time
 # Local imports
 import login_page
 import products_page
 
-driver = webdriver.Chrome()
-driver.get(login_page.url)
-time.sleep(2)
-assert login_page.hasLoaded(driver)
+class LoginTests(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Chrome()
+		self.fake = Faker()
+		self.driver.get(login_page.url)
+		self.assertTrue(login_page.hasLoaded(self.driver))
 
-login_page.username_input(driver).send_keys("standard_user")
-login_page.password_input(driver).send_keys("secret_sauce")
-time.sleep(2)
-login_page.login_button(driver).click()
+	def test_login_success(self):
+		login_page.attempt_login(self.driver, "standard_user", "secret_sauce")
+		self.assertTrue(products_page.hasLoaded(self.driver))
 
-time.sleep(2)
-assert products_page.hasLoaded(driver)
+	def test_login_failure(self):
+		login_page.attempt_login(self.driver, self.fake.email(), self.fake.text())
+		self.assertTrue(login_page.check_error_exists(self.driver))
+		self.assertTrue(login_page.verify_error_message(self.driver, "Username and password do not match any user"))
+		login_page.error_close_button(self.driver).click()
+		self.assertFalse(login_page.check_error_exists(self.driver))
 
-driver.quit()
+	def tearDown(self):
+		self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
--- products_page.py
+++ products_page.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time 
 
 url = "https://www.saucedemo.com/inventory.html"
 
